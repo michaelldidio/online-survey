4910efd0c7ce62aa02fcd7b31e03688629c831d1
michaelldidio <70228071+michaelldidio@users.noreply.github.com>
2022-06-26 23:28:05 -0400

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <div className="footer container">
+            <div>
+                <a href="/" className="title mr-1">WM Surveys</a>
+                <span>&copy; {new Date().getFullYear()}</span>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+
+
+function Home() {
+    return <div>
+        <div className="section">
+
+            <div className="content">
+                <h1>Welcome to WM Surveys!</h1>
+                <p>Create an account now to create a survey and share it with others to see their responses!</p>
+                <Link to="/create" className="btn">get started</Link>
+            </div>
+        </div>
+        <div className="section">
+
+
+        </div>
+    </div>
+}
+
+export default Home
